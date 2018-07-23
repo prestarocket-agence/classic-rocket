6ceb13ad513a188aa82e6ff6b44ea9de6e12d6a2
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2018-07-23 11:41:01 +0200

    Update webpack dependencies and everything around babel

--- _dev/webpack.config.js
+++ _dev/webpack.config.js
@@ -24,87 +24,84 @@
  */
 const webpack = require('webpack');
 const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 let config = {
-  entry: {
-    main: [
-      './js/theme.js',
-      './css/theme.scss'
-    ]
-  },
-  output: {
-    path: path.resolve(__dirname, '../assets/js'),
-    filename: 'theme.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                minimize: true
-              }
-            },
-            'postcss-loader',
-            'sass-loader'
-          ]
-        })
-      },
-      {
-        test: /.(png|woff(2)?|eot|ttf|svg|gif)(\?[a-z0-9=\.]+)?$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '../css/[hash].[ext]'
-            }
-          }
+    entry: {
+        main: [
+            './js/theme.js',
+            './css/theme.scss'
         ]
-      },
-      {
-        test : /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader']
-      }
-    ]
-  },
-  externals: {
-    prestashop: 'prestashop',
-    $: '$',
-    jquery: 'jQuery'
-  },
-  plugins: [
-    new ExtractTextPlugin(path.join('..', 'css', 'theme.css')),
-    new webpack.ProvidePlugin({
-        Popper: ['popper.js', 'default']
-    })
+    },
+    output: {
+        path: path.resolve(__dirname, '../assets/js'),
+        filename: 'theme.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                minimize: true
+                            }
+                        },
+                        'postcss-loader',
+                        'sass-loader'
+                    ]
+                })
+            },
+            {
+                test: /.(png|woff(2)?|eot|ttf|svg|gif)(\?[a-z0-9=\.]+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '../css/[hash].[ext]'
+                        }
+                    }
+                ]
+            },
+            {
+                test : /\.css$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader']
+            }
+        ]
+    },
+    externals: {
+        prestashop: 'prestashop',
+        $: '$',
+        jquery: 'jQuery'
+    },
+    plugins: [
+        new ExtractTextPlugin(path.join('..', 'css', 'theme.css')),
+        new webpack.ProvidePlugin({
+            Popper: ['popper.js', 'default']
+        })
   ]
 };
 
-config.plugins.push(
-  new webpack.optimize.UglifyJsPlugin({
-    sourceMap: true,
-    compress: {
-      sequences: true,
-      conditionals: true,
-      booleans: true,
-      if_return: true,
-      join_vars: true,
-      drop_console: true
-    },
-    output: {
-      comments: false
-    },
-    minimize: true
-  })
-);
+config.optimization = {
+    minimizer: [
+        // we specify a custom UglifyJsPlugin here to get source maps in production
+        new UglifyJsPlugin({
+            sourceMap: true,
+            parallel: true,
+            uglifyOptions: {
+                compress: false,
+                mangle: true
+            },
+        })
+    ]
+};
 
 module.exports = config;
