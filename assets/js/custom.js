45c35fd93961b88f5b82d74a032e0ed261438582
Kevin <kevin.moulun@prestarocket.com>
2019-07-11 15:08:39 +0200

    Add slick slider trigger

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,8 @@
+//Slick orientation trigger
+$(window).on("orientationchange",function(){
+    $('[data-slick]').slick('reinit');
+});
+
 /*
  * Custom code goes here.
  * A template should always ship with an empty custom.js
