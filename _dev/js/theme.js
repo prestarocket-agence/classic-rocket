35c76af9132a836e0897e760ef14a079d12fabda
Prestarocket <prestarocket@gmail.com>
2021-01-07 20:43:55 +0100

    add todo for custom file input

--- _dev/js/theme.js
+++ _dev/js/theme.js
@@ -44,6 +44,7 @@ $(document).ready(() => {
 
 
 //display input file content in custom file input BS
+// @todo still need ?
   $('.custom-file-input').on('change',function(){
       let fileName = $(this).val().split('\\').pop();
       $(this).next('.custom-file-label').addClass("selected").html(fileName);
