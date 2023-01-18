e40bbc1e631d656b2ef620f797d8e22257bdca0b
Léopold <leopold.manguette@prestarocket.com>
2023-01-18 11:27:11 +0100

    Update theme js

--- _dev/js/theme.js
+++ _dev/js/theme.js
@@ -12,50 +12,47 @@ import './product';
 import './cart';
 import './listing-add-to-cart';
 
+import prestashop from 'prestashop';
+import EventEmitter from 'events';
 
-import Form from './components/form';
-import TopMenu from './components/top-menu';
-import ProductMinitature from './components/product-miniature';
+import lazysizes from 'lazysizes'
+
 import './components/appearanceObserver';
+import './components/block-cart';
+import Form from './components/form';
+import ProductMinitature from './components/product-miniature';
+import TopMenu from './components/top-menu';
 import './components/sliders';
 import './components/touchspin';
 
 
-import prestashop from 'prestashop';
-import EventEmitter from 'events';
-
-
-import './components/block-cart';
-import lazysizes from 'lazysizes'
-
-
 // "inherit" EventEmitter
 for (var i in EventEmitter.prototype) {
-  prestashop[i] = EventEmitter.prototype[i];
+    prestashop[i] = EventEmitter.prototype[i];
 }
 
 $(document).ready(() => {
 
-  const form = new Form();
-  let productMinitature = new ProductMinitature();
+    const form = new Form();
+    let productMinitature = new ProductMinitature();
 
-  form.init();
+    form.init();
     let topMenuEl = $('#_desktop_top_menu #top-menu');
-    if(topMenuEl.length > 0) {
+    if (topMenuEl.length > 0) {
         let topMenu = new TopMenu(topMenuEl);
         topMenu.init();
     }
-  productMinitature.init();
+    productMinitature.init();
 
 
 //display input file content in custom file input BS
 // @todo still need ?
-  $('.custom-file-input').on('change',function(){
-      let fileName = $(this).val().split('\\').pop();
-      $(this).next('.custom-file-label').addClass("selected").text(fileName);
-  })
+    $('.custom-file-input').on('change', function () {
+        let fileName = $(this).val().split('\\').pop();
+        $(this).next('.custom-file-label').addClass("selected").text(fileName);
+    })
 });
 
-document.addEventListener('lazyloaded', function(e){
+document.addEventListener('lazyloaded', function (e) {
     $(e.target).parent().addClass('rc--lazyload');
 });
